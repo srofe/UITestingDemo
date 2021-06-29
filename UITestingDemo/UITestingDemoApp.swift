7120de8d935e057b6c984bedf6a31aec5a850688
Simon Rofe <simon.rofe@ieee.org>
2021-06-29 14:14:33 +1000

    Create new Xcode project

---
+++ UITestingDemo/UITestingDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  UITestingDemoApp.swift
+//  UITestingDemo
+//
+//  Created by Simon Rofe on 29/6/21.
+//
+
+import SwiftUI
+
+@main
+struct UITestingDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
