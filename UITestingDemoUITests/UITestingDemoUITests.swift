548bfeaa7b71a8439f3df4a112eaf5744c8df0ac
Simon Rofe <simon.rofe@ieee.org>
2021-06-29 16:00:22 +1000

    Refactor to remove duplication

--- UITestingDemoUITests/UITestingDemoUITests.swift
+++ UITestingDemoUITests/UITestingDemoUITests.swift
@@ -8,22 +8,20 @@
 import XCTest
 
 class UITestingDemoUITests: XCTestCase {
+    let app = XCUIApplication()
 
     override func setUpWithError() throws {
         continueAfterFailure = false
+        app.launch()
     }
 
     func testWelcome() throws {
-        let app = XCUIApplication()
-        app.launch()
         let welcome = app.staticTexts.element
         XCTAssert(welcome.exists, "There shall be a static text view.")
         XCTAssertEqual(welcome.label, "Welcome!", "The text view shall contain the text \"Welcome\".")
     }
 
     func testLoginButton() {
-        let app = XCUIApplication()
-        app.launch()
         let login = app.buttons["loginButton"]
         XCTAssert(login.exists, "There shall be a button with the accessibility identifier \"loginButton\".")
     }
