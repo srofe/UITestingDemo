7847e53eca8ce09ad94fe2082287a85a7250afe4
Simon Rofe <simon.rofe@ieee.org>
2021-06-30 09:52:42 +1000

    Add a user object to the environment

--- UITestingDemo/UITestingDemoApp.swift
+++ UITestingDemo/UITestingDemoApp.swift
@@ -9,9 +9,12 @@ import SwiftUI
 
 @main
 struct UITestingDemoApp: App {
+    var user = User()
+
     var body: some Scene {
         WindowGroup {
             WelcomeView()
+                .environmentObject(user)
         }
     }
 }
