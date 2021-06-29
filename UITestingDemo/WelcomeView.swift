af1767aea8db67a4cdad38cf4943f75571b8c791
Simon Rofe <simon.rofe@ieee.org>
2021-06-29 14:19:05 +1000

    Change view name

--- UITestingDemo/WelcomeView.swift
+++ UITestingDemo/WelcomeView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  WelcomeView.swift
 //  UITestingDemo
 //
 //  Created by Simon Rofe on 29/6/21.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct WelcomeView: View {
     var body: some View {
         Text("Hello, world!")
             .padding()
@@ -16,6 +16,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        WelcomeView()
     }
 }
