bb6823a6287e68948e9e0ff687a0672afd8d3618
Simon Rofe <simon.rofe@ieee.org>
2021-06-29 15:46:22 +1000

    Verify the content of the text view
    This now tests for the existence of the text view (with the first
    assertion) then tests the content of the text view is equal to
    "Welcome!".

--- UITestingDemoUITests/UITestingDemoUITests.swift
+++ UITestingDemoUITests/UITestingDemoUITests.swift
@@ -17,6 +17,7 @@ class UITestingDemoUITests: XCTestCase {
         let app = XCUIApplication()
         app.launch()
         let welcome = app.staticTexts.element
-        XCTAssert(welcome.exists, "There shall be a welcome text with the text \"Welcome!\".")
+        XCTAssert(welcome.exists, "There shall be a static text view.")
+        XCTAssertEqual(welcome.label, "Welcome!", "The text view shall contain the text \"Welcome\".")
     }
 }
