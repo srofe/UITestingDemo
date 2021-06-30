7eaa3255629e7075686d03c59d0ad2e5b20da265
Simon Rofe <simon.rofe@ieee.org>
2021-06-30 17:05:12 +1000

    Add login form

---
+++ UITestingDemo/LoginView.swift
@@ -0,0 +1,44 @@
+//
+//  LoginView.swift
+//  UITestingDemo
+//
+//  Created by Simon Rofe on 30/6/21.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    @EnvironmentObject private var user: User
+    @Environment(\.presentationMode) var presentationMode
+
+    var body: some View {
+        Form {
+            Section {
+                TextField("Username", text: $user.username)
+                SecureField("Password", text: $user.password)
+            }
+            Button {
+                if user.login() {
+                    presentationMode.wrappedValue.dismiss()
+                }
+            } label: {
+                Text("Login")
+            }
+            .accessibilityLabel("loginNow")
+        }
+        .navigationTitle(Text("Login"))
+        .navigationBarItems(trailing: Button {
+            presentationMode.wrappedValue.dismiss()
+        } label: {
+            Image(systemName: "xmark.circle")
+                .accessibilityLabel("Dismiss")
+        })
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+            .environmentObject(User())
+    }
+}
