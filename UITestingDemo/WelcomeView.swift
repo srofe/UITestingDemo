6fb5ab66896e51df5bf9d4fad8746deba8e64062
Simon Rofe <simon.rofe@ieee.org>
2021-07-06 11:39:59 +1000

    Add user configuration information to welcome page

--- UITestingDemo/WelcomeView.swift
+++ UITestingDemo/WelcomeView.swift
@@ -27,6 +27,41 @@ struct WelcomeView: View {
                 Text(user.isLoggedIn ? "Logout" : "Login")
             })
                 .accessibilityIdentifier("loginButton")
+            if user.isLoggedIn {
+                Form {
+                    Section {
+                        VStack {
+                            Text("Colour Theme")
+                            Picker("", selection: $user.colourTheme) {
+                                Text("Light").tag(0)
+                                Text("Dark").tag(1)
+                            }
+                            .pickerStyle(SegmentedPickerStyle())
+                            .accessibilityIdentifier("colourTheme")
+                        }
+                    }
+                    Section {
+                        HStack {
+                            Text("Text Size")
+                            Slider(value: $user.textSize, in: 1 ... 100)
+                                .accessibilityIdentifier("slider")
+                        }
+                    }
+                    Section {
+                        VStack {
+                            Text("Font")
+                            Picker("", selection: $user.font) {
+                                Text("Arial").tag("Arial")
+                                Text("Avenir Next").tag("Avenir Next")
+                                Text("Noteworthy").tag("Noteworthy")
+                                Text("Futura").tag("Futura")
+                            }
+                            .pickerStyle(WheelPickerStyle())
+                            .accessibilityIdentifier("fontPicker")
+                        }
+                    }
+                }
+            }
         }
         .sheet(isPresented: $showLogin) {
             LoginView()
@@ -37,5 +72,6 @@ struct WelcomeView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         WelcomeView()
+            .environmentObject(User())
     }
 }
